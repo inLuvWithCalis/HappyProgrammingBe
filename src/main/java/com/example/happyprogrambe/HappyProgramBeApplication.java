1c4433acda1bf08f2f09d242539605def5b9adfa
Min Thon Dang Nguyen <minhthongsoftengineer@gmail.com>
2025-07-09 09:10:55 +0700

    first commit

---
+++ src/main/java/com/example/happyprogrambe/HappyProgramBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.happyprogrambe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyProgramBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyProgramBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/happyprogrambe/HappyProgramBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.happyprogrambe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyProgramBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
